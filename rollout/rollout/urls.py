331a8b970c8dddd0cd295856ba6bd20d12109740
qiuwenjie <642288100@qq.com>
2023-12-25 22:20:42 +0800

    Revert "添加ppo"
    This reverts commit 823cc90f6e1744b3ef079df863c44e95bee1e28b.

--- rollout/rollout/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
-
-from rollout.views import RolloutView
-
-
-urlpatterns = patterns('',
-    url(r'^rollout\.js$', RolloutView.as_view(), name="rollout_js"),
-)
